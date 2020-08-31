cf507aa1deedcaf9952d50b05379dc1e40af1c4f
Henrique Pinho <henrique.de.nf@gmail.com>
2020-08-31 12:38:54 -0300

    removed comments

--- bisection_method.py
+++ bisection_method.py
@@ -1,39 +1,3 @@
-# from __future__ import division
-
-
-# def funcao(x0):
-#   return x0**4 - 3*x0**3 + 3
-
-# def bis(a, b, tol, n): # interval [a,b], tolerance, max number of iterations
-#   f0 = funcao(a)
-#   f1 = funcao(b)
-#   i = 1
-
-#   while ((abs(funcao(a)) > tol) and (abs(funcao(b)) > tol) and (i <= n)):
-#     if (abs(a - b) < tol):
-#       root = (a + b)/2
-#       average = (a + b)/2
-#       f2 = funcao(average)
-
-#       if (f0*f2 < 0):
-#         b = average
-#         f1 = funcao(average)
-#       else:
-#         a = average
-#         f0 = funcao(average)
-
-#       i = i + 1
-
-#       if (i > n):
-#         print('there was no convergence')
-#         if (abs(funcao(a)) < tol):
-#           root = a
-#         else:
-#           root = b
-
-
-# bis(0, 2, .01, 20)
-
 # Defining Function
 def f(x):
   return x**4 - 3*x**3 + 3
